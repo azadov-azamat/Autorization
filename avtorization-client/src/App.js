6cf41ff985bf9622b2939ab02accc3fbaf0b09e8
Azamat <73574661+azadov-azamat@users.noreply.github.com>
2021-06-17 14:08:38 +0500

    success all

--- avtorization-client/src/App.js
+++ avtorization-client/src/App.js
@@ -1,17 +1,17 @@
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import RegisterJs from "./components/pages/RegisterPage";
-import LoginJs from "./components/pages/LoginPage";
 import NotFound from "./components/pages/NotFound";
-
+import Pages from "./components/pages/Pages";
+import './components/style/main.scss';
 
 function App() {
   return (
       <>
           <Router>
               <Switch>
-                  <Route  path='/' component={RegisterJs}/>
-                  <Route  path='/signIn' component={LoginJs}/>
-                  <Route  component={NotFound}/>
+                  <Route exact path='/' component={Pages}/>
+                  <Route path='/signUp' component={RegisterJs}/>
+                  <Route component={NotFound}/>
               </Switch>
           </Router>
       </>
--- avtorization-server/src/main/java/com/example/avtorizationserver/AuthorizationServerApplication.java
+++ avtorization-server/src/main/java/com/example/avtorizationserver/AuthorizationServerApplication.java
@@ -1,13 +1,14 @@
 package com.example.avtorizationserver;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AvtorizationServerApplication {
+public class AuthorizationServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AvtorizationServerApplication.class, args);
+        SpringApplication.run(AuthorizationServerApplication.class, args);
     }
 
 }
